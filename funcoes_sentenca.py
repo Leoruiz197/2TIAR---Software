8488382c21fec37c14df0bc6cfc3b494532fb2a3
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2024-03-08 19:30:29 -0300

    Update funcoes_sentenca.py
    Predicado

--- funcoes_sentenca.py
+++ funcoes_sentenca.py
@@ -1,1 +1,15 @@
-# comecem por aqui
+def identificar_predicado(sujeito, sentenca):
+    posicao_da_palavra = sentenca.find(sujeito)
+    if posicao_da_palavra == -1:
+      return None
+    posicao_proxima_pontuacao = sentenca.find('.', posicao_da_palavra)
+    if posicao_proxima_pontuacao == -1:
+      return sentenca[posicao_da_palavra + len(sujeito):]
+    return sentenca[posicao_da_palavra + len(sujeito):posicao_proxima_pontuacao]
+ 
+ 
+# Exemplo de uso
+sujeito = "Joao"
+sentenca = "Joao esta procurando o pé de feijao"
+predicado = identificar_predicado(sujeito, sentenca)
+print("O predicado é:", predicado)# comecem por aqui
